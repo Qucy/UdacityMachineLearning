1b4c5e94ed285354171ab2aec24bc12bd9b26e42
ewwiqqu <qucy.qiu@ericsson.com>
2018-03-01 10:44:44 +0800

    update QLearner and mazeqlearning

--- qlearning_robot-master/QLearner.py
+++ qlearning_robot-master/QLearner.py
@@ -2,8 +2,8 @@
 Template for implementing QLearner
 """
 
-import numpy as np
 import random as rand
+import numpy as np
 
 class QLearner(object):
 
@@ -16,12 +16,16 @@ class QLearner(object):
         radr = 0.99,
         verbose = False):
 
-        #TODO
         self.verbose = verbose
+        self.num_states = num_states
         self.num_actions = num_actions
+        self.alpha = alpha
+        self.gamma = gamma
+        self.rar = rar
+        self.radr = radr
         self.s = 0
         self.a = 0
-        self.Qtable = np.zeros((self.s, self.a))
+        self.Qtable = np.zeros((self.num_states, self.num_actions))
 
     def querysetstate(self, s):
         """
@@ -30,11 +34,12 @@ class QLearner(object):
         @returns: The selected action
         """
         self.s = s
-        action = self.chooseAction(s)
+        action = self.choose_action(s)
+        self.a = action
         if self.verbose: print("s =", s,"a =",action)
         return action
 
-    # I don't tink query is a good name for this method since we need to update something
+
     def query(self,s_prime,r):
         """
         @summary: Update the Q table and return an action
@@ -43,20 +48,21 @@ class QLearner(object):
         @returns: The selected action
         """
         # choose action
-        action = self.chooseAction(s_prime)
+        action = self.choose_action(s_prime)
         if self.verbose: print("s =", s_prime,"a =",action,"r =",r)
         # update Q table
-        self.Qtable[self.s][action] = r + self.alhpa * self.Qtable[s_prime].argmax()
+        self.Qtable[self.s][self.a] = r + self.alpha * self.Qtable[s_prime].argmax()
         # update alpha, rar and state
         self.alpha *= self.gamma
         self.rar *= self.radr
         self.s = s_prime
+        self.a = action
 
         return action
 
-    def chooseAction(self, s):
-        p = rand.random()
-        if (self.rar > p):
+
+    def choose_action(self, s):
+        if (self.rar > rand.random()):
             return rand.randint(0, self.num_actions-1)
         else:
             return self.Qtable[s].argmax()
--- qlearning_robot-master/mazeqlearning.py
+++ qlearning_robot-master/mazeqlearning.py
@@ -2,6 +2,7 @@
 Train a Q Learner in a navigation problem.
 """
 
+import pandas as pd
 import numpy as np
 import random as rand
 import QLearner as ql
@@ -23,20 +24,39 @@ def to_state(pos):
 # each epoch involves one trip from start to the goal or timeout before reaching the goal
 # return list of rewards of each trip
 def train(maze, learner, epochs=500, timeout = 100000, verbose = False):
-    #TODO
     rewards = np.zeros(epochs)
+    goal = maze.get_goal_pos()
+    for i in range(epochs):
+        current_time = 0
+        total_reward = 0
+        robopos = maze.get_start_pos()
+        action = learner.querysetstate(to_state(robopos))
+        while (robopos != goal and current_time < timeout):
+            newpos, reward = maze.move(robopos, action)
+            if (-100 == reward):
+                print("epochs:", i, "S:", robopos, "A:", action, " S':", newpos, "R:", reward)
+            robopos = newpos
+            action = learner.query(to_state(robopos), reward)
+            total_reward += reward
+            current_time += 1
+        #print(i," reward:", total_reward)
+        rewards[i] = total_reward
+
+    print(rewards)
     return rewards
 
 
 # run the code to train a learner on a maze
 def maze_qlearning(filename):
-    #TODO
     #initialize maze object
+    data_frame = pd.read_csv(filename, header=None)
+    maze = Maze.Maze(data_frame.values, verbose=False)
     #initialize learner object
+    qlearner = ql.QLearner(verbose=False)
     #execute train(maze, learner)
+    rewards = train(maze, qlearner, epochs=1, timeout=1000)
     #return median of all rewards
-
-    return 0
+    return rewards
 
 if __name__=="__main__":
     rand.seed(5)
