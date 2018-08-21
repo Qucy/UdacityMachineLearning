bfd7bd636421f5e68f70e69619c38cababd569b4
Qucy <tracyqucy@gmail.com>
2018-08-21 14:02:09 +0800

    Add files via upload

---
+++ anomaly detection/frontend/src/app/model/hackathon.model.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class Hackathon { 
+    time: number = 0;
+    cpu: number = 0;
+    network: number = 0;
+}
+    
---
+++ anomaly detection/frontend/src/app/model/test.model.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class HackathonTest {  
+    time: string = "";
+    cpu: number = 0;
+    cpuRange:string = "";
+    network: number = 0;
+    networkRange:string = "";
+    expect: number = 1;
+    predict: number;
+}
+    
