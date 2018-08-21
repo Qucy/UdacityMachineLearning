a0f0fe4f046420b49aca8f9771729b46ded6be75
Qucy <tracyqucy@gmail.com>
2018-08-21 13:46:38 +0800

    Update app.py

--- anomaly detection/app.py
+++ anomaly detection/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, request
 from flask import Response
 
+import os
 import json
 import h5py
 import numpy as np
@@ -23,7 +24,7 @@ rand = np.random.RandomState(42)
 @app.route('/train1', methods=['POST'])
 def train_normal():
     # retrieve data from request
-    cpu, network, time = json.loads(preprocessData(str(request.data)))
+    cpu, network, time = preprocessData(str(request.data))
     # retrieve train features
     train_features = generateTrainFeatures(cpu, network)
     # train model
@@ -33,6 +34,8 @@ def train_normal():
     joblib.dump(model, 'model.pkl')
     # save features
     storeTrainFeatures(train_features)
+    # plot model
+    plot_data_in_different_angle(train_features, None, None)
 
     return "OK", 200
 
@@ -40,7 +43,7 @@ def train_normal():
 def train_with_business_grow():
     train_features = None
     # loading data from request
-    cpu, network, time = json.loads(preprocessData(str(request.data)))
+    cpu, network, time = preprocessData(str(request.data))
     # looping generate plot imsages
     for i in range(1, 17):
         train_features = generateTrainFeatures(cpu + i, network + i)
@@ -53,7 +56,7 @@ def train_with_business_grow():
     # save features
     storeTrainFeatures(train_features)
 
-    return "OK"
+    return "OK", 200
 
 @app.route('/benchmark', methods=['POST'])
 def benchmark():
@@ -80,22 +83,22 @@ def customize_predict():
     if model == None:
         return "Please train model first!!"
     # retreive data from request
-    cpu, network, time = json.loads(preprocessData(str(request.data)))
+    cpu, network, time = preprocessData(str(request.data))
     # generate test features
     test_features = generateSpecifyTestFeatures(cpu, network, time)
     # do predict
     predictions = predict(test_features)
     # plot data
-    plot_data_in_different_angle(loadTrainFeatures(), test_features, predictions)
+    plot_data(loadTrainFeatures(), test_features=test_features, predictions=predictions)
 
     return str(predictions[0]), 200
 
 def generateTestFeatures():
     test_features = []
     # normal data at 3:00
-    test_features.append(np.array([15, 15, retrieveHourOfDay(3)]))
+    #test_features.append(np.array([15, 15, retrieveHourOfDay(3)]))
     # anormaly data at 03:00
-    test_features.append(np.array([30, 30, retrieveHourOfDay(3)]))
+    #test_features.append(np.array([35, 35, retrieveHourOfDay(3)]))
 
     # normal data at 8:00
     test_features.append(np.array([25, 25, retrieveHourOfDay(8)]))
@@ -113,50 +116,53 @@ def generateTestFeatures():
     test_features.append(np.array([40, 40, retrieveHourOfDay(20)]))
 
     # normal data at 23:00
-    test_features.append(np.array([15, 15, retrieveHourOfDay(23)]))
+    #test_features.append(np.array([15, 15, retrieveHourOfDay(23)]))
     # anormaly data at 23:00
-    test_features.append(np.array([30, 30, retrieveHourOfDay(23)]))
+    #test_features.append(np.array([35, 35, retrieveHourOfDay(23)]))
 
-    test_features = np.reshape(test_features, (10, 3))
+    test_features = np.reshape(test_features, (6, 3))
 
     return test_features
 
 def generateTrainFeatures(cpu=10, network=10):
     # idle data from 01:00 to 07:00
-    cpu_load_1 = generateCPULoadOrNetwork(cpu, cpu + 10, 350)
-    net_work_1 = generateCPULoadOrNetwork(network, network + 10, 350)
-    hourofday_1 = generateTimeRange(1, 7, 50)
+    cpu_load_1 = generateCPULoadOrNetwork(cpu, cpu + 10, 800)
+    net_work_1 = generateCPULoadOrNetwork(network, network + 10, 800)
+    hourofday_1 = generateTimeRange(0, 7, 100)
 
     # growing data from 07:00 to 10:00
-    cpu_load_2 = generateCPULoadOrNetwork(cpu + 10, cpu + 20, 200)
-    net_work_2 = generateCPULoadOrNetwork(network + 10, network + 20, 200)
-    hourofday_2 = generateTimeRange(7, 10, 50)
+    cpu_load_2 = generateCPULoadOrNetwork(cpu + 10, cpu + 20, 400)
+    net_work_2 = generateCPULoadOrNetwork(network + 10, network + 20, 400)
+    hourofday_2 = generateTimeRange(7, 10, 100)
 
     # busy data from 10:00 to 19:00
-    cpu_load_3 = generateCPULoadOrNetwork(cpu + 20, cpu + 30, 500)
-    net_work_3 = generateCPULoadOrNetwork(network + 20, network + 30, 500)
-    hourofday_3 = generateTimeRange(10, 19, 50)
+    cpu_load_3 = generateCPULoadOrNetwork(cpu + 20, cpu + 30, 1000)
+    net_work_3 = generateCPULoadOrNetwork(network + 20, network + 30, 1000)
+    hourofday_3 = generateTimeRange(10, 19, 100)
 
     # decrease data from 19:00 to 21:00
-    cpu_load_4 = generateCPULoadOrNetwork(cpu + 10, cpu + 20, 150)
-    net_work_4 = generateCPULoadOrNetwork(network + 10, network + 20, 150)
-    hourofday_4 = generateTimeRange(19, 21, 50)
+    cpu_load_4 = generateCPULoadOrNetwork(cpu + 10, cpu + 20, 300)
+    net_work_4 = generateCPULoadOrNetwork(network + 10, network + 20, 300)
+    hourofday_4 = generateTimeRange(19, 21, 100)
 
     # idle data from 21:00 to 24:00
-    cpu_load_5 = generateCPULoadOrNetwork(cpu, cpu + 10, 200)
-    net_work_5 = generateCPULoadOrNetwork(network, network + 10, 200)
-    hourofday_5 = generateTimeRange(21, 24, 50)
+    cpu_load_5 = generateCPULoadOrNetwork(cpu, cpu + 10, 400)
+    net_work_5 = generateCPULoadOrNetwork(network, network + 10, 400)
+    hourofday_5 = generateTimeRange(21, 24, 100)
 
     # concate busy, growing, decrease and idle data
     cpu_load = np.r_[cpu_load_1, cpu_load_2, cpu_load_3, cpu_load_4, cpu_load_5]
     net_work = np.r_[net_work_1, net_work_2, net_work_3, net_work_4, net_work_5]
-    hourofday = np.reshape(np.r_[hourofday_1, hourofday_2, hourofday_3, hourofday_4, hourofday_5], (1400, 1))
+    hourofday = np.reshape(np.r_[hourofday_1, hourofday_2, hourofday_3, hourofday_4, hourofday_5], (2900, 1))
     # stack data as feature matrix
     train_features = np.hstack((cpu_load, net_work, hourofday))
     return train_features
 
 
 def storeTrainFeatures(train_features):
+    # delete file if already exist
+    if os.path.isfile(train_features_filename):
+        os.remove(train_features_filename)
     with h5py.File(train_features_filename) as h:
         h.create_dataset("train", data=train_features)
 
@@ -169,11 +175,10 @@ def loadTrainFeatures():
 def preprocessData(data):
     data = data.replace("\'", "")
     data = data.replace("b", "")
+    data = json.loads(data)
     cpu = data['cpu']
     network = data['network']
-    time = 0
-    if (data['time'] is not None):
-        time = data['time']
+    time = data['time']
     return cpu, network, time
 
 def generateSpecifyTestFeatures(cpu, network, time):
@@ -212,9 +217,9 @@ def plot_data(normal_features, test_features=None, predictions=None, angle=None,
             ax.scatter(curr_time, curr_cpu, curr_network, c=retrieveColor(pred), marker='o')
     if angle is not None:
         ax.view_init(30, 140 + angle)
-        filename = 'plot' + str(int(angle/10 - 4)) + '.jpg'
+        filename = 'static/plot' + str(int(angle/10 - 4)) + '.jpg'
     else:
-        filename = 'plot' + str(idx) + '.jpg'
+        filename = 'static/plot' + str(idx) + '.jpg'
     plt.savefig(filename)
 
 def plot_data_in_different_angle(features, test_features, prediction):
